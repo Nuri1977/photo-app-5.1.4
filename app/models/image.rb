4da36b0d7d54a928ac4ab5346e79ce5ece2ce1f0
nuri1977 <nurilacka@gmail.com>
2020-02-02 16:31:30 +0100

    Image size validations

--- app/models/image.rb
+++ app/models/image.rb
@@ -1,4 +1,13 @@
 class Image < ApplicationRecord
   belongs_to :user
   mount_uploader :picture, PicturesUploader
+  validate :picture_size
+
+  private
+
+  def picture_size
+    if picture.size > 5.megabytes
+      errors.add(:picture, "shouls be less than 5MB")
+    end
+  end
 end
--- app/uploaders/pictures_uploader.rb
+++ app/uploaders/pictures_uploader.rb
@@ -1,4 +1,6 @@
 class PicturesUploader < CarrierWave::Uploader::Base
+  include CarrierWave::MiniMagick
+  process resize_to_limit: [300, 300]
   # Include RMagick or MiniMagick support:
   # include CarrierWave::RMagick
   # include CarrierWave::MiniMagick
@@ -35,9 +37,9 @@ class PicturesUploader < CarrierWave::Uploader::Base
 
   # Add a white list of extensions which are allowed to be uploaded.
   # For images you might use something like this:
-  # def extension_whitelist
-  #   %w(jpg jpeg gif png)
-  # end
+  def extension_whitelist
+    %w(jpg jpeg gif png)
+  end
 
   # Override the filename of the uploaded files:
   # Avoid using model.id or version_name here, see uploader/store.rb for details.
